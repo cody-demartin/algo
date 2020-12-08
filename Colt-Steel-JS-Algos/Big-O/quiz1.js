38f771593bb2e06be7209a6300eb49eaff99e325
cdemarti <codythomasdemartin@gmail.com>
2020-12-08 16:26:58 -0500

    Quiz 1: score 5/5

---
+++ Colt-Steel-JS-Algos/Big-O/quiz1.js
@@ -0,0 +1,19 @@
+// 1. Simplify the big O expression as much as possible: O(n + 10)
+//    A: O(n)
+//    R: correct
+
+// 2. Simplify the big O expression as much as possible: O(100 * n)
+//    A: O(n)
+//    R: correct
+
+// 3. Simplify the big O expression as much as possible: O(25)
+//    A: O(1)
+//    R: correct
+
+// 4. Simplify the big O expression as much as possible: O(n^2 + n^3)
+//    A: O(n^3)
+//    R: correct
+
+// Simplify the big O expression as much as possible: O(n + n + n + n)
+//    A: O(n)
+//    R: correct
