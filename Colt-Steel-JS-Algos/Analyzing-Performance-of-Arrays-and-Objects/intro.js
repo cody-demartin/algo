75545f159c848d9d38ca1e7ef35884364c37303e
cdemarti <codythomasdemartin@gmail.com>
2020-12-09 13:00:23 -0500

    Intro on objects

---
+++ Colt-Steel-JS-Algos/Analyzing-Performance-of-Arrays-and-Objects/intro.js
@@ -0,0 +1,40 @@
+//  Objectives
+//      - Understand how objects amd arrays work    
+//      - Explain why adding elements to the beginning of an array is costly
+//      - Compare and contrast the runtime for array and objects, as well as built in methods
+
+//  Objects
+//      Unordered data structure
+//      Key value pairs
+//          Work well when you do not need order
+//          When you need fast access/insertion and removal
+//              - constant time for insertion removal and accessing data
+//              - searching is o(n)
+//                  searching is defined as searching for a particular value within a hash which is why its o(n)
+//              - Big O of Object Methods
+//                  Object.keys - O(n)
+//                  Object.values - O(n)
+//                  Object.entries - O(n)
+//                  hasOwnProperty - O(1)
+//      
+//
+//      
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
