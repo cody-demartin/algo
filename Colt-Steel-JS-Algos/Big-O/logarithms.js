01ac800a7a595ec6bae02991d48b9eb2f04f3c68
cdemarti <codythomasdemartin@gmail.com>
2020-12-09 12:32:00 -0500

    Finished first section. Second section creation.

---
+++ Colt-Steel-JS-Algos/Big-O/logarithms.js
@@ -0,0 +1,17 @@
+//  Logarithms (math)
+//      Some big O expressions that involve more complex math
+//          - log(n)
+//
+//  What is a log again?
+//      log2(8) = 3
+//          "2 to what power is eight?"
+//              Since 2^3 = 8, the value of log2(8) is 3
+//
+//  The logarithm of a number roughly measures the number of times you can divide that number by 2 
+//  before you get a value that's less than or equal to one
+//
+// Example:
+//      8 / 2 => 4 - 1
+//      4 / 2 => 2 - 2
+//      2 / 2 => 1 - result: 3 
+//  Efficient sorting methods involve logs and recursion sometimes involves log space complexity
