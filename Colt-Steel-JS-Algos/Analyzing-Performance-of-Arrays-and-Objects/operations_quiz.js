689f8ad4a63a2314a1e0cf62b8b5e9505e0cfc47
cdemarti <codythomasdemartin@gmail.com>
2020-12-09 13:29:41 -0500

    finished section 3, begin section 4

---
+++ Colt-Steel-JS-Algos/Analyzing-Performance-of-Arrays-and-Objects/operations_quiz.js
@@ -0,0 +1,9 @@
+// 2. What is the big O for accessing a key in an object?
+//      A: O(1)
+//      R: Correct
+//
+// 3. What is the big O of removing a key in an obbject?
+//      A: O(1)
+//      R: Correct
+//
+// First question was about creatin keys, also O(1)
