7e2caf97a75ff897b15f3a6e7b74660d7c4aef6f
cdemarti <codythomasdemartin@gmail.com>
2020-12-14 14:11:05 -0500

    Divide and conq notes

---
+++ Colt-Steel-JS-Algos/Problem-Solving-Approach/divide_and_conq.js
@@ -0,0 +1,15 @@
+//  Divide and conq
+//
+//  This pattern involves dividing a data set into smaller chunks and then repeating
+//  a process with a subset of data. 
+//
+//  This pattern can tremendously reduce time complexity. 
+//
+//
+//  An Example:
+//      Given a sorted array of integers, write a function called search, that accepts a 
+//      value and returns the index where the value passed to the function is located. If
+//      the value is not found, return -1
+//
+//
+//      Linear search vs binary search
