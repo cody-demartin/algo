a12b6410ced1aa618bb189b2ec78e379dcde3f55
cdemarti <codythomasdemartin@gmail.com>
2021-02-08 22:14:08 -0500

    adds stack intro notes

---
+++ Colt-Steel-JS-Algos/Data-Stuctures/Stacks/intro.js
@@ -0,0 +1,18 @@
+// What is a stack?
+//  Define what a stack is
+//  Understand what it is
+
+// A stack is an abstract data structure that is a collection of data that 
+// follows a LIFO (last in first out) data processing philosophy
+
+// The last element added to the stack will be the first element removed from
+// the stack
+
+// A stack of plates as it piles up the topmost thing is what gets removed
+// the first
+
+// First, last, size
+// Used in undo/redo in photoshop/text editors
+// History objects in your browser, i.e. routing in React
+// Stacks in algorithms
+
